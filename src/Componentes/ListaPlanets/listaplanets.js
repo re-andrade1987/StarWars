953c9df000dfdd6d92d74580b3bc2ea012ed334a
re-andrade1987 <renataandrade1987@hotmail.com>
2020-01-24 01:54:17 -0300

    Melhorando teste

--- src/Componentes/ListaPlanets/listaplanets.js
+++ src/Componentes/ListaPlanets/listaplanets.js
@@ -1,5 +1,5 @@
-import axios from 'axios';
 import React from 'react';
+import planets from '../../services/planets'
 import './Lista.css';
 
 export default class StarWars extends React.Component {
@@ -7,34 +7,36 @@ export default class StarWars extends React.Component {
         super(props)
         this.state = {
             planets: [],
-           
+            films: [],
+            error: false,
+            loading: ''
         }
         this.getData = this.getData.bind(this)
     }
 
     getData() {
-        const randomNumber = Math.floor((Math.random() * 88 + 1))
-        let apiUrl = "https://swapi.co/api/planets" + randomNumber
-
-        axios.get(apiUrl)
-        .then(response => response.data.results)
-    }
-
-    componentDidMount() {
-        const responseInfoPlanets = this.getData('planets');
-        responseInfoPlanets.then(planets => {
-            console.log(planets)
-            this.setState({ planets })
+        let idPlanet = Math.floor((Math.random() * 88 + 1))
+        planets.getPlanet(idPlanet).then(planet => {
+            this.setState({
+                planet: planet.data,
+                loading: false,
+                error: planet.ok
+            })
+        }).catch(error => {
+            this.setState({
+                loading: false,
+                error: false
+            })
         })
     }
 
-    handleClick(){
-       console.log('Fui clicado')
-       this.getData()
+    componentDidMount() {
+        this.getData()
     }
+
     render() {
         return (
-            <div  className="main">
+            <div className="main">
                 <div className='info'>
                     <ul>
                         <div className='first-li'>
@@ -43,8 +45,8 @@ export default class StarWars extends React.Component {
                         <li>Population: {this.state.population}</li>
                         <li>Climate: {this.state.climate}</li>
                         <li>Terrain: {this.state.population}</li>
-                        <p>Featured in films</p>
-                        <button onClick={this.handleClick}>Next</button>
+                        <div className="filmes">Featured in {this.state.films.length} FILMS</div>
+                        <button onClick={this.getData}>Next</button>
                     </ul>
                 </div>
             </div>
---
+++ src/services/planets.js
@@ -0,0 +1,9 @@
+import api from '../services/api'
+
+export default {
+    async getPlanet(planetId) {
+        const response = await api.get(`/planets/${planetId}/`)
+        console.log(response)
+        return response
+    }
+}
